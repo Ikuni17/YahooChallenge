f8ce4b1e5c61ee535f7698ae969b8c1813839caa
Ikuni17 <white.brad17@gmail.com>
2023-04-03 10:30:14 -0600

    Remove unnecessary api

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
 import {MantineProvider} from '@mantine/core';
-import {QueryClient, QueryClientProvider} from 'react-query';
 import {defaultTheme} from './styles/theme';
 import {Router} from './routes';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false
-    }
-  }
-});
-
 export const App: React.FC = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <MantineProvider withGlobalStyles withNormalizeCSS theme={defaultTheme}>
-        <Router />
-      </MantineProvider>
-    </QueryClientProvider>
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={defaultTheme}>
+      <Router />
+    </MantineProvider>
   );
 };
