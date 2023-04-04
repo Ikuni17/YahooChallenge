1f74dbfab45b18691e11a0c96a6b5ec93e3221e0
Ikuni17 <white.brad17@gmail.com>
2023-04-03 18:07:04 -0600

    Use HashRouter

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import {Route, Switch, BrowserRouter} from 'react-router-dom';
+import {Route, Switch, HashRouter} from 'react-router-dom';
 import {Routes} from './routes';
 import {Home} from '../views/Home';
 import {NotFound} from '../views/NotFound';
 
 export const Router: React.FC<{children?: React.ReactNode}> = ({children}) => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       {children}
       <Switch>
         <Route exact path={Routes.Home} component={Home} />
         <Route component={NotFound} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
